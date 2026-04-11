b7c30402d11b6f40c119be4150574f2bb7dc71c2
Nguyen Duc Tien <neittien0110@gmail.com>
2026-04-11 16:11:26 +0700

    Adder Tree cho khớp thần kinh synapse

--- SNN_SingleLIF.srcs/sources_1/new/synapse_unit.v
+++ SNN_SingleLIF.srcs/sources_1/new/synapse_unit.v
@@ -5,6 +5,7 @@
  * @param VMEM_WIDTH Độ rộng bus dữ liệu của hệ thống.
  */
 module synapse_unit #(
+    parameter ARCH = "FAST",
     parameter VMEM_WIDTH = 16,
     parameter WEIGHT_0   = 8'd24,
     parameter WEIGHT_1   = 8'd20,
@@ -14,11 +15,21 @@ module synapse_unit #(
     input  wire [3:0]           input_spikes,
     output wire [VMEM_WIDTH-1:0] total_energy
 );
-
-    // Sử dụng logic chọn (Mux) để lấy Weight nếu có Spike, ngược lại lấy 0
-    assign total_energy = (input_spikes[0] ? WEIGHT_0 : {VMEM_WIDTH{1'b0}}) +
-                          (input_spikes[1] ? WEIGHT_1 : {VMEM_WIDTH{1'b0}}) +
-                          (input_spikes[2] ? WEIGHT_2 : {VMEM_WIDTH{1'b0}}) +
-                          (input_spikes[3] ? WEIGHT_3 : {VMEM_WIDTH{1'b0}});
-
+    generate
+        if (ARCH == "ORI") begin : arch_original
+            // Sử dụng logic chọn (Mux) để lấy Weight nếu có Spike, ngược lại lấy 0
+            assign total_energy = (input_spikes[0] ? WEIGHT_0 : {VMEM_WIDTH{1'b0}}) +
+                                  (input_spikes[1] ? WEIGHT_1 : {VMEM_WIDTH{1'b0}}) +
+                                  (input_spikes[2] ? WEIGHT_2 : {VMEM_WIDTH{1'b0}}) +
+                                  (input_spikes[3] ? WEIGHT_3 : {VMEM_WIDTH{1'b0}});
+        end else begin : arch_fast
+            // Bảo đảm cấu trúc bộ Adder Tree (Cây bộ cộng) được tạo ra.
+            wire [VMEM_WIDTH-1:0] total_01, total_23;
+            assign total_01 = (input_spikes[0] ? WEIGHT_0 : {VMEM_WIDTH{1'b0}}) +
+                            (input_spikes[1] ? WEIGHT_1 : {VMEM_WIDTH{1'b0}});
+            assign total_23 = (input_spikes[2] ? WEIGHT_2 : {VMEM_WIDTH{1'b0}}) +
+                            (input_spikes[3] ? WEIGHT_3 : {VMEM_WIDTH{1'b0}});
+            assign total_energy = total_01 + total_23;
+        end 
+    endgenerate
 endmodule
